7d89853f4759177b09df54a3915a01b522fb3000
Lucas da Silva Rosito <lucasrosito000@gmail.com>
2026-05-24 22:53:13 -0300

    adicionando exercicios (2 a 7)

---
+++ mundo-01/ex004.py
@@ -0,0 +1,9 @@
+resposta_usuario = input('Digite algo: ')
+print(f'''o tipo primitivo desse valor é {type(resposta_usuario)} 
+Só tem espaços? {resposta_usuario.isspace()}
+É um número? {resposta_usuario.isnumeric()}
+É alfabético? {resposta_usuario.isalpha()}
+É alfanúmerico? {resposta_usuario.isalnum()}
+Está em maiúsculas? {resposta_usuario.isupper()}
+Está em minúsculas? {resposta_usuario.islower()}
+Está capitalizada?? {resposta_usuario.istitle()}''') 
---
+++ mundo-01/ex006.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um número inteiro: '))
+print(f'''O dobro de {num} vale {num * 2}.
+O triplo de {num} vale {num * 3}.
+A raiz quadrada de {num} vale {num ** (1/2):.2f}''')
